6d6232962ee172a757806a2fce151a501774e9be
George Fleming <seorasproductions@gmail.com>
2023-02-06 12:34:51 +0000

    2 points completed

--- cash-register.js
+++ cash-register.js
@@ -1,8 +1,42 @@
 function cashRegister(price, cash, cid) {
     
     // let result = {status: "", change []}
+    let drawer = {
+        "PENNY": 1.01,
+        "NICKEL": 2.05,
+        "DIME": 3.1,
+        "QUARTER": 4.25,
+        "ONE": 90,
+        "FIVE": 55,
+        "TEN": 20,
+        "TWENTY": 60,
+        "ONE HUNDRED": 100
+    };
+    // declare counters - one for amount in till, one for change due
+    let change = cash*100 - price*100;
+    let totalCID = 0;
+ 
+    for (let elem of cid) {
+        totalCID += elem[1]*100
+    }
+    if (change > totalCID) {
+        return {
+            status: "INSUFFICIENT_FUNDS", change: []
+        }
+    } if (price > cash) {
+        return {
+            status: "INCORRECT_PAYMENT", change: []
+        }
+    } else if (change === totalCID) {
+        return {
+            status: "CLOSED", change: [cid]
+        }
+    }
+
+
+
     
-    // declare counters - one for amount in till, one for amount left in the till
+    
 
     // Create variable for calculation of diff between cash, price - if (conditional) cash given < price. Status "Incorrect Payment" plus empty array for change.
     
